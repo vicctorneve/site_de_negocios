5a4c1096210690158e317e5970cf7d27d42b92b5
vicctorneves <vicctorneves88@gmail.com>
2022-08-05 08:03:12 -0300

    ✨ feat: Animação scroll

---
+++ js/scrool-animate.js
@@ -0,0 +1,17 @@
+const itens = document.querySelectorAll('[data-anime]')
+console.log(itens)
+
+function animateScroll(){
+   const windowTop = window.scrollY + window.innerHeight * 0.85;
+   itens.forEach(element => {
+      if(windowTop > element.offsetTop){
+         element.classList.add('animate')
+      } else {
+         element.classList.remove('animate')
+      }
+   });
+}
+
+window.addEventListener('scroll', function(){
+   animateScroll()
+})
